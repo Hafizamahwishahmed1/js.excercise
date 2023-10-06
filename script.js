f39dd400916804c653683ccca3df3aaeffe42de7
Hafizamahwishahmed1 <143023442+Hafizamahwishahmed1@users.noreply.github.com>
2023-10-07 01:04:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+// Practice exercise 1.1
+
+console.log("13+10");
+console.log("Hi!");
+console.log("05");
+
+// Practice exercise 1.2
+
+alert("Welcome there!");
+alert("Hello World!");
+console.log("Welcome to JavaScript!");
+
+
+// Practice exercise 1.3
+
+alert("Saying hi from other file!");
+alert("Hi there!");
+
+// Practice exercise 1.4
+
+let a = 20;
+
+console.log("20");
+console.log("50");
+console.log("44");
+
+
+// Prompt
+
+prompt("Welcome Sir! How are you?");
+
+// Multiple-line comment.
+
+// console.log("Hi!");
+// console.log("Welcome to css!");
+// console.log("05");
+// console.log("42");
+// alert("Hi there!");
+// alert("Hello!");
+
+
+// Self-check quiz
+
+
+// 1. <script type="text/javascript" src="./script.js"></script>
+
+// 2. To execute JavaScript in a browser you have two options — either put it inside a script element anywhere inside an HTML document, or put it inside an external JavaScript file (with a . js extension) and then reference that file inside the HTML document using an empty script element with a src attribute.
+
+// 3. Multi-line comments in JavaScript code can be added by writing the comment between a forward-slash followed by asterisk /* and asterisk followed by a forward-slash */ . Below are some examples of multi-line comments.
+
+
+
+
